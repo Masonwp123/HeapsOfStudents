840270a4f7aa218c17fd695b1aee6b421cbb4f29
Masonwp123 <masonparker868@gmail.com>
2025-02-17 15:58:44 +0000

    removed testing from main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-#include "student.h"
 
 int main() {
 	std::cout << "Hello World!" << std::endl;
-	Student* student = new Student;
-	student->sayHi();
-	delete student;
 	return 0;
 }
